5da9cccf926b4605a8a52829a21ccd6ceb36e0fb
PappuPanaut <170117104+PappuPanaut@users.noreply.github.com>
2024-10-18 23:38:04 +0530

    Create bi_lift.rs

---
+++ src/bi_lift.rs
@@ -0,0 +1,25 @@
+mod up {
+    pub struct BiLift {
+        up: Vec<Vec<u32>>,
+    }
+
+    impl BiLift {
+        pub fn new(pa: Vec<u32>) -> Self {
+            let mut up = vec![pa];
+
+            for k in 0..32 {
+                up.push((0..up[0].len()).map(|u| up[k][up[k][u] as usize]).collect());
+            }
+
+            Self { up }
+        }
+
+        pub fn kth(&self, u: usize, k: usize) -> usize {
+            if k == 0 {
+                return u;
+            }
+
+            self.kth(self.up[k.trailing_zeros() as usize][u] as _, k & (k - 1))
+        }
+    }
+}
