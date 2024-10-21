c5ed513272231fb30fbcc1a95896c28bac614e4f
PappuPanaut <170117104+PappuPanaut@users.noreply.github.com>
2024-10-21 20:29:08 +0530

    Update bi_lift.rs

--- src/bi_lift.rs
+++ src/bi_lift.rs
@@ -1,15 +1,63 @@
 mod up {
     #[derive(Clone, Debug)]
     pub struct BiLift {
-        up: Vec<Vec<u32>>,
+        up: Vec<Vec<usize>>,
     }
 
     impl BiLift {
-        pub fn new(pa: Vec<u32>) -> Self {
+        pub fn new(pa: Vec<usize>) -> Self {
             let mut up = vec![pa];
 
             for k in 0..32 {
-                up.push(up[k].iter().map(|&u| up[k][u as usize]).collect());
+                up.push(up[k].iter().map(|&u| up[k][u]).collect());
+            }
+
+            Self { up }
+        }
+
+        pub fn kth(&self, u: usize, k: u32) -> usize {
+            if k == 0 {
+                return u;
+            }
+
+            self.kth(self.up[k.trailing_zeros() as usize][u] as _, k & (k - 1))
+        }
+
+        pub fn lca(&self, mut u: usize, mut v: usize, dep: &[u32]) -> usize {
+            if dep[u] < dep[v] {
+                return self.lca(v, u, dep);
+            }
+
+            u = self.kth(u, dep[u] - dep[v]);
+
+            if u == v {
+                return v;
+            }
+
+            for k in (0..33).rev() {
+                if self.up[k][u] == self.up[k][v] {
+                    continue;
+                }
+
+                u = self.up[k][u] as _;
+                v = self.up[k][v] as _;
+            }
+
+            self.up[0][u] as _
+        }
+    }
+}mod up {
+    #[derive(Clone, Debug)]
+    pub struct BiLift {
+        up: Vec<Vec<usize>>,
+    }
+
+    impl BiLift {
+        pub fn new(pa: Vec<usize>) -> Self {
+            let mut up = vec![pa];
+
+            for k in 0..32 {
+                up.push(up[k].iter().map(|&u| up[k][u]).collect());
             }
 
             Self { up }
